aa77b33200f2e58c820a9d5a3a42062fda9a75a3
JeffGillispie <email@jeffgillispie.com>
2017-03-13 15:54:36 -0500

    Added properties to DocumentSet.

--- LoadFileAdapter/DocumentSet.cs
+++ LoadFileAdapter/DocumentSet.cs
@@ -10,10 +10,107 @@ namespace LoadFileAdapter
     public class DocumentSet : IEnumerable<Document>
     {
         private List<Document> docs = new List<Document>();
+        private int imageCount = -1;
+        private int textCount = -1;
+        private int nativeCount = -1;
+        private int parentCount = -1;
+        private int childCount = -1;
+        private int standAloneCount = -1;
         
-        public void AddDocuments(List<Document> docs)
+        public void AddDocuments(IEnumerable<Document> docs)
         {
-            this.docs.AddRange(docs);            
+            this.docs.AddRange(docs);
+            
+            if (this.imageCount != -1)
+            {
+                countValues();
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.docs.Count;
+            }
+        }
+
+        public int ImageCount
+        {
+            get
+            {
+                if (this.imageCount == -1)
+                {
+                    countValues();
+                }
+
+                return this.imageCount;
+            }
+        }
+
+        public int TextCount
+        {
+            get
+            {
+                if (this.textCount == -1)
+                {
+                    countValues();
+                }
+
+                return this.textCount;
+            }
+        }
+
+        public int NativeCount
+        {
+            get
+            {
+                if (this.nativeCount == -1)
+                {
+                    countValues();
+                }
+
+                return this.nativeCount;
+            }
+        }
+
+        public int ParentCount
+        {
+            get
+            {
+                if (this.parentCount == -1)
+                {
+                    countValues();
+                }
+
+                return this.parentCount;
+            }
+        }
+
+        public int ChildCount
+        {
+            get
+            {
+                if (this.childCount == -1)
+                {
+                    countValues();
+                }
+
+                return this.childCount;
+            }
+        }
+
+        public int StandAloneCount
+        {
+            get
+            {
+                if (this.standAloneCount == -1)
+                {
+                    countValues();
+                }
+
+                return this.standAloneCount;
+            }
         }
 
         public Document this[int index]
@@ -28,63 +125,57 @@ namespace LoadFileAdapter
                 this.docs[index] = value;
             }
         }
-        /*
-        public Document First()
-        {
-            return this.docs.First();
-        }
-
-        public Document Last()
-        {
-            return this.docs.Last();
-        }
-
-        private class DocEnumerator : IEnumerator
-        {
-            public List<Document> docs;
-            int position = -1;
-
-            public DocEnumerator(List<Document> docs)
-            {
-                this.docs = docs;
-            }
-
-            private IEnumerator<Document> getEnumerator()
-            {
-                return (IEnumerator<Document>)this;
-            }
-
-            public bool MoveNext()
-            {
-                this.position++;
-                return (this.position < this.docs.Count);
-            }
-
-            public void Reset()
-            {
-                this.position = -1;
-            }
-
-            object IEnumerator.Current
-            {
-                get
-                {                    
-                    return this.docs[this.position];                    
-                }
-            }
-        }        
-        */
-
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            //return new DocEnumerator(this.docs);
             return this.docs.GetEnumerator();
         }
 
         IEnumerator<Document> IEnumerable<Document>.GetEnumerator()
         {
             return this.docs.GetEnumerator();
+        }
+
+        private void countValues()
+        {
+            this.imageCount = 0;
+            this.textCount = 0;
+            this.nativeCount = 0;
+            this.parentCount = 0;
+            this.childCount = 0;
+            this.standAloneCount = 0;
+
+            foreach (Document doc in this.docs)
+            {
+                foreach (var rep in doc.Representatives)
+                {
+                    if (rep.RepresentativeType == Representative.Type.Image)
+                    {
+                        imageCount += rep.Files.Count;
+                    }
+                    else if (rep.RepresentativeType == Representative.Type.Native)
+                    {
+                        nativeCount += rep.Files.Count;
+                    }
+                    else if (rep.RepresentativeType == Representative.Type.Text)
+                    {
+                        textCount += rep.Files.Count;
+                    }
+                }
+
+                if (doc.Parent != null)
+                {
+                    childCount++;
+                }
+                else if (doc.Children != null)
+                {
+                    parentCount++;
+                }
+                else
+                {
+                    standAloneCount++;
+                }
+            }
         }
     }
 }
