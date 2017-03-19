8c9ad7abd4f41ef5bdce2247ac3fe9bcccec2caf
JeffGillispie <email@jeffgillispie.com>
2017-03-19 09:23:05 -0500

    Added class documentation.

--- LoadFileAdapter/Document.cs
+++ LoadFileAdapter/Document.cs
@@ -2,14 +2,42 @@ using System.Collections.Generic;
 
 namespace LoadFileAdapter
 {
+    /// <summary>
+    /// Represents a document from a load file. Holds the document key, parent, 
+    /// a list of children, a collection of metadata key / value pairs, and a
+    /// set of all file representations of that document.
+    /// </summary>
     public class Document
     {
+        /// <summary>
+        /// The document key or DOCID value of this document.
+        /// </summary>
         private string key;
+        /// <summary>
+        /// The parent document value of this document.
+        /// </summary>
         private Document parent;
+        /// <summary>
+        /// A list of child documents of this document.
+        /// </summary>
         private List<Document> children;
+        /// <summary>
+        /// A collection of metadata key / value pairs belonging to this document.
+        /// </summary>
         private Dictionary<string, string> metadata;
+        /// <summary>
+        /// The set of file representations for this document.
+        /// </summary>
         private HashSet<Representative> representatives;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Document"/> class.
+        /// </summary>
+        /// <param name="key">The document key or DOCID.</param>
+        /// <param name="parent">The parent document.</param>
+        /// <param name="children">A list of child documents.</param>
+        /// <param name="metadata">A collection of metadata key / value pairs.</param>
+        /// <param name="representatives">A set of file representations of this document.</param>
         public Document(string key, 
             Document parent, 
             List<Document> children, 
@@ -23,6 +51,9 @@ namespace LoadFileAdapter
             this.representatives = representatives;
         }
 
+        /// <summary>
+        /// The document key or DOCID value of the document.
+        /// </summary>
         public string Key
         {
             get
@@ -31,6 +62,9 @@ namespace LoadFileAdapter
             }            
         }
 
+        /// <summary>
+        /// The parent document.
+        /// </summary>
         public Document Parent
         {
             get
@@ -39,6 +73,9 @@ namespace LoadFileAdapter
             }            
         }
 
+        /// <summary>
+        /// A list of child documents.
+        /// </summary>
         public List<Document> Children
         {
             get
@@ -47,6 +84,9 @@ namespace LoadFileAdapter
             }            
         }
 
+        /// <summary>
+        /// A collection of metadata key / value pairs. Where the key is the metadat field name and the value is the field value.
+        /// </summary>
         public Dictionary<string, string> Metadata
         {
             get
@@ -55,6 +95,10 @@ namespace LoadFileAdapter
             }
         }
 
+        /// <summary>
+        /// The set of all file representations of the document.
+        /// These can be of the type <see cref="Representative.Type"/>.
+        /// </summary>
         public HashSet<Representative> Representatives
         {
             get
@@ -63,6 +107,10 @@ namespace LoadFileAdapter
             }
         }
 
+        /// <summary>
+        /// Sets the parent of the document.
+        /// </summary>
+        /// <param name="parent">The parent document.</param>
         public void SetParent(Document parent)
         {
             this.parent = parent;
@@ -78,9 +126,13 @@ namespace LoadFileAdapter
             }
         }
         
+        /// <summary>
+        /// Sets the representatives of the document.
+        /// </summary>
+        /// <param name="reps">A set of all file representations of the document.</param>
         public void SetRepresentatives(HashSet<Representative> reps)
         {
             this.representatives = reps;
-        }
+        }        
     }
 }
